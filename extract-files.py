e2f024d3784ba7c5a122ac1187508df25ba15b62
kylieeXD <khayloaftck@gmail.com>
2026-05-20 21:09:58 +0700

    miuicamera: Apply patch for MiuiCamera.apk
    Signed-off-by: kylieeXD <khayloaftck@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -44,6 +44,8 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libgui_shim_miuicamera.so'),
     'system_ext/lib64/vendor.mediatek.hardware.camera.isphal-V1-ndk.so': blob_fixup()
         .replace_needed('android.hardware.graphics.common-V5-ndk.so', 'android.hardware.graphics.common-V7-ndk.so'),
+    'system_ext/priv-app/MiuiCamera/MiuiCamera.apk': blob_fixup()
+        .apktool_patch('patches'),
 }
 
 module = ExtractUtilsModule(
